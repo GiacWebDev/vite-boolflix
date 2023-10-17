64159dce661a7c33b9b87d67dcd88810c3c82aea
GiacWebDev <giacobechini@gmail.com>
2023-10-17 17:07:28 +0200

    [edit] language flags

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -8,7 +8,6 @@ export default {
       Vote: Number,
       Overview: String
   }
-  
 }
 </script>
 
@@ -27,7 +26,36 @@ export default {
       <h1>{{ Title }}</h1>
       <h4>{{ OriginalTitle }}</h4>
 
-      <h5>{{ Language }}</h5>
+      <h5><strong>Lingua: </strong>
+        <span v-if="Language == 'en'">
+            <img :src="'https://cdn.icon-icons.com/icons2/97/PNG/256/united_kingdom_flags_flag_17079.png'"
+                :alt="Language">
+        </span>
+
+        <span v-else-if="Language == 'es'">
+            <img :src="'https://cdn.icon-icons.com/icons2/97/PNG/256/spain_flags_flag_17068.png'"
+                :alt="Language">
+        </span>
+
+        <span v-else-if="Language == 'it'">
+            <img :src="'https://cdn.icon-icons.com/icons2/97/PNG/256/italy_flags_flag_17018.png'"
+                :alt="Language">
+        </span>
+
+        <span v-else-if="Language == 'fr'">
+            <img :src="'https://cdn.icon-icons.com/icons2/97/PNG/256/france_flags_flag_16999.png'"
+                :alt="Language">
+        </span>
+
+        <span v-else-if="Language == 'de'">
+            <img :src="'https://cdn.icon-icons.com/icons2/97/PNG/256/germany_flags_flag_17001.png'"
+                :alt="Language">
+        </span>
+      
+        <span v-else>{{ Language }}</span>
+      </h5>
+
+
       <h5>Voto: {{ Vote }}</h5>
 
       <div class="overview">
@@ -42,5 +70,9 @@ export default {
 
 <style lang="scss" scoped>
 @use '../../assets/scss/main.scss';
+
+img {
+  width: 25px;
+}
 
 </style>
